0cca792e787d55aac26f7f1af8cc8318e0043834
djole1993 <djordje_srdic@hotmail.com>
2017-08-08 10:41:05 +0200

    Initial Commit

---
+++ MTB Cycle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MTB Cycle
+//
+//  Created by 30hills on 8/8/17.
+//  Copyright © 2017 Djordje Srdic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
